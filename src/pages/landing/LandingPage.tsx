6872b834c7fdb2569f71dbab46d5b749be4df741
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-20 23:11:07 +0200

    Fixed subtitle.

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <img src={logo} alt="sTUMatch Logo" width="70%" />
         <Heading as="h2" size="md" mt="8" color="primary" opacity="0.8" fontWeight="normal" lineHeight={1.5}>
           Meet other students, get notified about events at your university and build long-lasting relationships - all
-          one location.
+          in one location.
         </Heading>
       </Stack>
       <Box>
