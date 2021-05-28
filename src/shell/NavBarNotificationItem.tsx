62d111b7322618f480fc995b4180f4972e478a33
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-28 21:46:45 +0200

    Added basic unread notifications indicator.

--- src/shell/NavBarNotificationItem.tsx
+++ src/shell/NavBarNotificationItem.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Center,
   Divider,
   Popover,
@@ -20,17 +21,29 @@ import { Link as RouterLink } from 'react-router-dom';
 import { routes } from '../constants';
 import { NoNotificationsEmptyState } from '../components/EmptyStates';
 import range from 'lodash-es/range';
+import take from 'lodash-es/take';
 import NotificationTemplateSkeleton from '../components/NotificationTemplateSkeleton';
 
 export default function NavBarNotificationItem() {
-  const { isLoading, data } = useGetAllUserNotificationsQuery(me, { page: 1, pageSize: 20, sort: 'createdOn:desc' });
+  const { isLoading, data } = useGetAllUserNotificationsQuery(me, { page: 1, pageSize: 100, sort: 'createdOn:desc' });
+  const hasUnreadNotifications = data?.result.some((x) => !x.seen) ?? false;
+  // The above unread notifications boolean reflects only the latest 100 notifications.
+  // This is a good enough tradeoff which doesn't require additional endpoints on the
+  // backend for querying the number while also providing the user with a mostly accurate
+  // value (as the change for having an unread notification in the first 100 results is
+  // more than likely).
 
   return (
     <Popover isLazy>
       {({ onClose }) => (
         <>
           <PopoverTrigger>
-            <IconButton aria-label="Notifications" variant="ghost" icon={<IoNotificationsOutline />} />
+            <Box pos="relative">
+              <IconButton aria-label="Notifications" variant="ghost" icon={<IoNotificationsOutline />} />
+              {hasUnreadNotifications && (
+                <Box rounded="full" bg="primary.500" w="2" h="2" position="absolute" top="1.5" right="1.5" />
+              )}
+            </Box>
           </PopoverTrigger>
           <PopoverContent w={['xs', 'lg']} h={['xs', 'lg']}>
             <PopoverArrow />
@@ -46,7 +59,7 @@ export default function NavBarNotificationItem() {
             {data && data.result.length > 0 && (
               <>
                 <VStack divider={<StackDivider />} spacing="0" overflowY="auto">
-                  {data?.result.map((notification) => (
+                  {take(data.result, 20).map((notification) => (
                     <NotificationSelector key={notification.id} notification={notification} />
                   ))}
                 </VStack>
