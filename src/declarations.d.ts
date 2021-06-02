96f8ef0dd11e4ac0fdbb813dc480b4453d4f6017
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-06-02 12:39:10 +0200

    Improved socket integration into app.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,4 @@ declare module '*.png';
 declare module '*.svg';
 
 declare const API_BASE_URL: string;
+declare const WS_BASE_URL: string;
---
+++ src/sockets/socket.ts
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+
+export interface AppSocketContextProps {
+  /**
+   * The application's shared socket that is connected to the backend.
+   *
+   * This is `undefined` if no socket connection has been established (yet).
+   * This is, for example, the case when no user is authenticated yet.
+   */
+  socket?: Socket;
+}
+
+/**
+ * React context for accessing the application's shared socket that is connected to the backend.
+ */
+export const AppSocketContext = createContext<AppSocketContextProps>({});
+
+/**
+ * Establishes a WS connection to the backend given the specified token and returns the resulting connected socket.
+ *
+ * This returns `undefined` if no socket connection has been established (yet).
+ * This is, for example, the case when no user is authenticated yet.
+ * @param token The token to be used for WS authentication.
+ * @returns The connected socket instance once
+ */
+export function useSocket(token?: string) {
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+
+  useEffect(() => {
+    if (token) {
+      const socket = connectSocket(token);
+      socket.on('connect', () => console.info('(Re-)connected to the WS.'));
+      socket.on('disconnect', () => console.warn('Disconnected from the WS.'));
+      setSocket(socket);
+    }
+
+    return () => {
+      socket?.disconnect();
+    };
+  }, [token]);
+
+  return socket;
+}
+
+function connectSocket(token: string) {
+  const socket = io(WS_BASE_URL, {
+    path: '/ws',
+    extraHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return socket;
+}
