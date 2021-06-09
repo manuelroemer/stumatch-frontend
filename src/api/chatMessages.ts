fd673e78ac1dfda1d03481db83b9ef61838ab5db
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-06-09 19:15:41 +0200

    Added chat messages API call.

--- src/api/chatMessages.ts
+++ src/api/chatMessages.ts
@@ -1,8 +1,14 @@
+import { ApiResult } from './apiResult';
 import { ApiObject } from './conventions';
+import { stumatchFetch, StumatchFetchInit } from './fetch';
 
 export interface ChatMessage extends ApiObject {
   chatGroupId: string;
   userId: string;
   textContent: string;
   isDeleted: boolean;
+}
+
+export function getAllChatGroupChatMessages(chatGroupId: string, init?: StumatchFetchInit) {
+  return stumatchFetch<ApiResult<ChatMessage>>(`/api/v1/chatGroups/${chatGroupId}/chatMessages`, init);
 }
