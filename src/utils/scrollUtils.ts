7493da870ea5c4ec14d47f5e4eeac8ba36698186
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-06-13 17:20:01 +0200

    Remove unused function.

--- src/utils/scrollUtils.ts
+++ src/utils/scrollUtils.ts
@@ -2,10 +2,6 @@ export function scrollToBottom(element: Element) {
   element.scrollTop = element.scrollHeight;
 }
 
-export function isScrolledToTop(element: Element) {
-  return element.scrollTop === 0;
-}
-
 export function isScrolledToBottom(element: HTMLElement) {
   return element.scrollTop + element.offsetHeight === element.scrollHeight;
 }
