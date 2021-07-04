0cdf18fd3e2f70a708e9450100c74aa2a1ea5419
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-07-04 16:12:55 +0200

    Allow multi-line input with SHIFT+Enter.

--- src/pages/chat/ChatMessageInput.tsx
+++ src/pages/chat/ChatMessageInput.tsx
@@ -1,30 +1,36 @@
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
-import { HStack, IconButton, Input, Tooltip } from '@chakra-ui/react';
+import { ChangeEvent, KeyboardEvent, useRef, useState } from 'react';
+import { HStack, IconButton, Textarea, Tooltip } from '@chakra-ui/react';
 import { IEmojiData } from 'emoji-picker-react';
 import { BiSend } from 'react-icons/bi';
 import isEmpty from 'lodash-es/isEmpty';
 import trim from 'lodash-es/trim';
+import countBy from 'lodash-es/countBy';
 import EmojiPickerButton from './EmojiPickerPopover';
 
 export default function ChatMessageInput() {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
   const [typedMessage, setTypedMessage] = useState('');
   const sanitizedMessage = trim(typedMessage);
   const canSubmit = !isEmpty(sanitizedMessage);
+  const typedLines = countBy(typedMessage)['\n'] + 1 || 1;
 
   const handleEmojiSelected = (emoji: IEmojiData) => {
     setTypedMessage(typedMessage + emoji.emoji);
     inputRef.current!.focus();
   };
 
-  const handleTypedMessageChanged = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleTypedMessageChanged = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setTypedMessage(e.target.value);
   };
 
-  const handleSend = (e: FormEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleTextAreaKeyPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      send();
+    }
+  };
 
+  const send = () => {
     // Should be checked as the handler can be invoked with the Enter key despite the button being disabled.
     if (!canSubmit) {
       return;
@@ -34,34 +40,41 @@ export default function ChatMessageInput() {
   };
 
   return (
-    <form onSubmit={handleSend} action="none">
-      <HStack
-        spacing="4"
-        py="4"
-        px="8"
-        bg="white"
-        overflowY="scroll"
-        // Dirty hack: To get the correct padding as the chat messages above we need the system's
-        // scrollbar width. Simply achieve that by adding a transparent scrollbar.
-        sx={{ scrollbarColor: 'transparent transparent' }}
-        css={{
-          '::-webkit-scrollbar': {
-            backgroundColor: 'transparent',
-          },
-        }}>
-        <EmojiPickerButton onEmojiSelected={handleEmojiSelected} />
-        <Input ref={inputRef} rounded="full" autoFocus value={typedMessage} onChange={handleTypedMessageChanged} />
-        <Tooltip type="submit" label="Send" hasArrow>
-          <IconButton
-            aria-label="Send"
-            icon={<BiSend />}
-            colorScheme="primary"
-            rounded="full"
-            type="submit"
-            isDisabled={!canSubmit}
-          />
-        </Tooltip>
-      </HStack>
-    </form>
+    <HStack
+      spacing="4"
+      py="4"
+      px="8"
+      bg="white"
+      overflowY="scroll"
+      // Dirty hack: To get the correct padding as the chat messages above we need the system's
+      // scrollbar width. Simply achieve that by adding a transparent scrollbar.
+      sx={{ scrollbarColor: 'transparent transparent' }}
+      css={{
+        '::-webkit-scrollbar': {
+          backgroundColor: 'transparent',
+        },
+      }}>
+      <EmojiPickerButton onEmojiSelected={handleEmojiSelected} />
+      <Textarea
+        ref={inputRef}
+        rounded="3xl"
+        autoFocus
+        resize="none"
+        rows={Math.min(5, typedLines)}
+        value={typedMessage}
+        onKeyPress={handleTextAreaKeyPress}
+        onChange={handleTypedMessageChanged}
+      />
+      <Tooltip type="submit" label="Send" hasArrow>
+        <IconButton
+          aria-label="Send"
+          icon={<BiSend />}
+          colorScheme="primary"
+          rounded="full"
+          isDisabled={!canSubmit}
+          onClick={send}
+        />
+      </Tooltip>
+    </HStack>
   );
 }
