842e942d4979c8153c2c94116199e5e0f13b6025
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-07-05 17:12:28 +0200

    Added chat message menu.

--- src/pages/chat/ChatMessageSelector.tsx
+++ src/pages/chat/ChatMessageSelector.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
 import ReactTimeago from 'react-timeago';
 import { ChatMessage } from '../../api/chatMessages';
 import { useCurrentUser } from '../../stores/userStore';
+import { HiChevronDown } from 'react-icons/hi';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 
 export interface ChatMessageSelectorProps {
   chatMessage: ChatMessage;
@@ -10,17 +12,37 @@ export interface ChatMessageSelectorProps {
 export default function ChatMessageSelector({ chatMessage }: ChatMessageSelectorProps) {
   const currentUser = useCurrentUser();
   const isWrittenByCurrentUser = chatMessage.userId === currentUser.id;
+  const textColor = isWrittenByCurrentUser ? 'white' : undefined;
 
   return (
     <Flex justify={isWrittenByCurrentUser ? 'flex-end' : 'flex-start'}>
-      <Box
-        maxW="60%"
-        my="0.5"
-        p="2"
-        rounded="2xl"
-        bg={isWrittenByCurrentUser ? 'primary.500' : 'gray.200'}
-        color={isWrittenByCurrentUser ? 'white' : 'black'}>
-        <Text whiteSpace="pre-line">{chatMessage.textContent}</Text>
+      <Box maxW="60%" my="0.5" p="2" rounded="2xl" bg={isWrittenByCurrentUser ? 'primary.500' : 'gray.200'}>
+        <Flex alignItems="flex-start">
+          <Text flexGrow={1} minW={0} whiteSpace="pre-line" color={textColor}>
+            {chatMessage.textContent}
+          </Text>
+          <Menu strategy="fixed">
+            <MenuButton
+              mt="1"
+              ml="1"
+              minH="6"
+              as={IconButton}
+              aria-label="Options"
+              icon={<HiChevronDown />}
+              size="xs"
+              variant="link"
+              color={textColor}
+              opacity={0.6}
+              _hover={{ opacity: 0.8 }}
+              _active={{ color: textColor, opacity: 1.0 }}
+              _focus={{ outline: 'none' }}
+            />
+            <MenuList>
+              <MenuItem icon={<AiOutlineEdit />}>Edit</MenuItem>
+              <MenuItem icon={<AiOutlineDelete />}>Delete</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
         <ReactTimeago
           minPeriod={1}
           date={chatMessage.modifiedOn}
