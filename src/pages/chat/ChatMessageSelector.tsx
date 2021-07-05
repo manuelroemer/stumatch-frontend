faf0802be0782c394bc95811c7e76657e3d8bcfb
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-07-05 15:31:08 +0200

    Added message inline time.

--- src/pages/chat/ChatMessageSelector.tsx
+++ src/pages/chat/ChatMessageSelector.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
+import ReactTimeago from 'react-timeago';
 import { ChatMessage } from '../../api/chatMessages';
 import { useCurrentUser } from '../../stores/userStore';
 
@@ -20,6 +21,20 @@ export default function ChatMessageSelector({ chatMessage }: ChatMessageSelector
         bg={isWrittenByCurrentUser ? 'primary.500' : 'gray.200'}
         color={isWrittenByCurrentUser ? 'white' : 'black'}>
         <Text whiteSpace="pre-line">{chatMessage.textContent}</Text>
+        <ReactTimeago
+          minPeriod={1}
+          date={chatMessage.modifiedOn}
+          component={(props) => (
+            <Text
+              mt="1"
+              float="right"
+              fontSize="0.6rem"
+              color={isWrittenByCurrentUser ? 'white' : 'black'}
+              opacity={0.6}
+              {...props}
+            />
+          )}
+        />
       </Box>
     </Flex>
   );
