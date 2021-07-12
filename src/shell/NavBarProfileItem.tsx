ea4bdb09d0117aee80ed2740dcb1ad17d898d540
Quoc Khang Phan <ga45wod@mytum.de>
2021-07-13 01:33:18 +0200

    addressed issues in review

--- src/shell/NavBarProfileItem.tsx
+++ src/shell/NavBarProfileItem.tsx
@@ -12,71 +12,51 @@ import {
   Text,
   VStack,
   StackDivider,
-  Link,
   Button,
   PopoverFooter,
   PopoverBody,
-  Avatar,
-  HStack,
   Flex,
-  Spacer,
 } from '@chakra-ui/react';
 import { IoPersonCircleOutline } from 'react-icons/io5';
-import { me } from '../api/conventions';
-import NotificationSelector from '../components/NotificationSelector';
-import { useGetAllUserNotificationsQuery, useNotificationsSocketQueryInvalidation } from '../queries/notifications';
-import { Link as RouterLink } from 'react-router-dom';
-import { routes } from '../constants';
-import { NoNotificationsEmptyState } from '../components/EmptyStates';
-import range from 'lodash-es/range';
-import ImageTitleDescriptionSkeleton from '../components/ImageTitleDescriptionSkeleton';
 import { useCurrentUser, useUserStore } from '../stores/userStore';
+import UserAvatar from '../components/UserAvatar';
+import { getFullName } from '../utils/userUtils';
 
 const pageSize = 20;
 
 export default function NavBarProfileItem() {
-  const { isLoading, data } = useGetAllUserNotificationsQuery(me, { page: 1, pageSize, sort: 'createdOn:desc' });
-  const hasUnreadNotifications = data?.result.some((x) => !x.seen) ?? false;
-  const hiddenNotifications = data ? data.totalCount - pageSize : -1;
-  useNotificationsSocketQueryInvalidation();
-
   const logout = useUserStore((state) => state.logout);
   const user = useCurrentUser();
-  const userName = user.firstName + ' ' + user.lastName;
 
   return (
     <Popover isLazy strategy="fixed">
-      {({ onClose }) => (
-        <>
-          <PopoverTrigger>
-            <Box pos="relative">
-              <IconButton aria-label="Profile" variant="ghost" icon={<IoPersonCircleOutline />} />
-            </Box>
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverHeader>Profile</PopoverHeader>
-            <PopoverBody>
-              <Center p="3">
-                <Avatar name={userName} />
-              </Center>
-              <Divider />
-              <VStack p="3">
-                <Text>{userName}</Text>
-                <Text>{user.email}</Text>
-                <StackDivider />
-                <Button variant="link">Show Profile</Button>
-              </VStack>
-            </PopoverBody>
-            <PopoverFooter p="2">
-              <Flex justify="flex-end">
-                <Button onClick={logout}>Logout</Button>
-              </Flex>
-            </PopoverFooter>
-          </PopoverContent>
-        </>
-      )}
+      <PopoverTrigger>
+        <Box pos="relative">
+          <IconButton aria-label="Profile" variant="ghost" icon={<IoPersonCircleOutline />} />
+        </Box>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Profile</PopoverHeader>
+        <PopoverBody>
+          <Center p="3">
+            <UserAvatar />
+          </Center>
+          <Divider />
+          <VStack p="3">
+            <Text>{getFullName(user)}</Text>
+            <Text>{user.email}</Text>
+            <StackDivider />
+            <Button variant="link">Show Profile</Button>
+          </VStack>
+        </PopoverBody>
+        <PopoverFooter p="2">
+          <Flex justify="flex-end">
+            <Button onClick={logout}>Logout</Button>
+          </Flex>
+        </PopoverFooter>
+      </PopoverContent>
     </Popover>
   );
 }
